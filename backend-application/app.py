624f26255361c7c90ce3725b1b803d2ac2938147
ivoilieviliev <i.iliev1316@gmail.com>
2022-06-11 18:46:25 +0300

    Add backend functionality to write.read datai into/from mysql db

--- backend-application/app.py
+++ backend-application/app.py
@@ -1,4 +1,5 @@
 import py_compile
+import mysql.connector
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,3 +8,44 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
+
+@app.route('/write-name')
+def write():
+    db = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        passwd = "supersecret",
+        database = "company"
+    )
+    cursor = db.cursor()
+    query = "INSERT INTO employees (first_name, last_name, department, email ) VALUES (%s, %s, %s, %s)"
+    values = ("Georgi", "Georgiev", "Sales", "g.georgiev@mail.com")
+    cursor.execute(query, values)
+    db.commit()
+
+    print(cursor.rowcount, "record inserted")
+    cursor.close()
+    return "User has been added"
+
+@app.route('/read-name')
+def read():
+    db = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        passwd = "supersecret",
+        database = "company"
+    )
+    cursor = db.cursor(dictionary=True)
+    query = "SELECT * FROM employees"
+    cursor.execute(query)
+    records = cursor.fetchall()
+    count = cursor.rowcount
+    
+    rowCount = range(count)
+    dicts = {}
+    for i in rowCount:
+        for record in records:
+            dicts[i+1] = record
+    cursor.close()
+    
+    return dicts
